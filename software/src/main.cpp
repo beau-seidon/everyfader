8e38fa2104f9ce3e047403e350d4e33ee89f0c4e
Beau Sterling <beau.seidon@outlook.com>
2024-06-05 23:38:25 -0500

    release 1.0

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -2,10 +2,18 @@
 #include <USB-MIDI.h>
 
 
+void read_pot();
+void update_cc_value(byte);
+void read_buttons();
+void change_LED_mode();
+void change_cc(int);
+void update_LEDs();
+
+
 const int PIN_POT = A0;
 const int PIN_UP_BTN = 20;
 const int PIN_DN_BTN = 19;
-const int LED_PINS[7] = { 10, 3, 5, 6, 9, 7, 8 };
+const int LED_PINS[7] = { 10, 9, 6, 5, 3, 8, 7 };
 
 int pot_raw_value = 0;
 int pot_prev_value = 0;
@@ -27,7 +35,7 @@ unsigned long t_mode;
 byte midi_channel = 1;
 
 byte cc_active = 20;
-byte cc_values[127];
+byte cc_values[128];
 
 enum MODE { CC, CC_VAL, ERROR };
 MODE LED_mode = CC;
@@ -35,18 +43,11 @@ bool LED_mode_latch = false;
 uint16_t LED_mode_debounce_period = 500;
 
 
-void read_pot();
-void update_cc_value(byte);
-void read_buttons();
-void change_LED_mode();
-void change_cc(int);
-void update_LEDs();
-
-
 USBMIDI_CREATE_DEFAULT_INSTANCE();  // SparkFun Pro Micro native USB MIDI
 
 
-void setup() {
+void setup()
+{
 /* set pin modes */
     pinMode(PIN_UP_BTN, INPUT_PULLUP);
     pinMode(PIN_DN_BTN, INPUT_PULLUP);
@@ -55,15 +56,16 @@ void setup() {
 /* initialize CC values */
     for (int i = 0; i < 127; i++) cc_values[i] = 64;
 
-/* start midi listener */
-    MIDI.begin(midi_channel);
+/* start midi listeners */
+    MIDI.begin(MIDI_CHANNEL_OMNI);
 
 /* initialize timers */
     t = t_up = t_dn = t_mode = millis();
 }
 
 
-void loop() {
+void loop()
+{
     t = millis();
     read_pot();
     read_buttons();
@@ -71,8 +73,9 @@ void loop() {
 }
 
 
-void read_pot() {
-    analogRead(PIN_POT);  // charge the input
+void read_pot()
+{
+    analogRead(PIN_POT);  // charge the input cap
     pot_raw_value = analogRead(PIN_POT);
     if (abs(pot_prev_value - pot_raw_value) > pot_deadband) {  // prevent ADC jitter and noise
         pot_prev_value = pot_raw_value;
@@ -82,7 +85,8 @@ void read_pot() {
 }
 
 
-void update_cc_value(byte val) {
+void update_cc_value(byte val)
+{
     if (cc_values[cc_active] == val) cc_value_latch = false;  // don't change CC value after changing CC number until fader matches CC value (prevents large jumps)
     if (!cc_value_latch) {
         cc_values[cc_active] = val;
@@ -91,7 +95,8 @@ void update_cc_value(byte val) {
 }
 
 
-void read_buttons() {
+void read_buttons()
+{
     up_state = !digitalRead(PIN_UP_BTN);
     dn_state = !digitalRead(PIN_DN_BTN);
 
@@ -126,7 +131,8 @@ void read_buttons() {
 }
 
 
-void change_LED_mode() {
+void change_LED_mode()
+{
     switch (LED_mode) {
         case CC:
             LED_mode = CC_VAL;
@@ -141,7 +147,8 @@ void change_LED_mode() {
 }
 
 
-void change_cc(int inc) {
+void change_cc(int inc)
+{
     int cc = cc_active;  // dummy value for clean inc/dec
     cc += inc;
     if (cc < 0) cc = 127;  // wrap around
@@ -151,7 +158,8 @@ void change_cc(int inc) {
 }
 
 
-void update_LEDs() {
+void update_LEDs()
+{
     switch (LED_mode) {
         case CC:  // show 7-bit binary representation of MIDI Control number
             for (int i = 0; i < 7; i++) digitalWrite(LED_PINS[i], (cc_active & (1 << i)));
@@ -171,5 +179,4 @@ void update_LEDs() {
         default:  // error
             break;
     }
-
 }
